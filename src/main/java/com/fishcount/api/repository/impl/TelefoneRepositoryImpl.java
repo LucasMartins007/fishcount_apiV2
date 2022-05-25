f2a343a7180fc7ab5ffe0862a5a3db414fa62e70
Lucas Martins <lucasrodriguesmartinsoliveira@gmail.com>
2022-05-25 16:15:33 -0300

    Correção importação de telefoneSpec

--- src/main/java/com/fishcount/api/repository/impl/TelefoneRepositoryImpl.java
+++ src/main/java/com/fishcount/api/repository/impl/TelefoneRepositoryImpl.java
@@ -1,8 +1,8 @@
 package com.fishcount.api.repository.impl;
 
+import com.fishcount.api.infrastructure.spec.TelefoneSpec;
 import com.fishcount.api.repository.custom.CustomTelefoneRepository;
 import com.fishcount.api.repository.dao.GenericImpl;
-import com.fishcount.api.infrastructure.context.spec.TelefoneSpec;
 import com.fishcount.common.model.entity.Telefone;
 import com.fishcount.common.model.entity.Usuario;
 import com.fishcount.common.model.enums.EnumTipoTelefone;
