d3b5ca709565a0867eecba16411a3dc48ee8b8a0
Lucas Martins <lucasrodriguesmartinsoliveira@gmail.com>
2022-07-07 09:03:03 -0300

    Ajuste para autenticacaoDTO retornar o id da pessoa do usuario logado

--- src/main/java/com/fishcount/api/service/PessoaService.java
+++ src/main/java/com/fishcount/api/service/PessoaService.java
@@ -3,10 +3,13 @@ package com.fishcount.api.service;
 import com.fishcount.api.service.impl.interfaces.IAbstractService;
 import com.fishcount.common.model.dto.PessoaDTO;
 import com.fishcount.common.model.entity.Pessoa;
+import com.fishcount.common.model.entity.Usuario;
 
 public interface PessoaService extends IAbstractService<Pessoa, Integer, PessoaDTO> {
 
     Pessoa incluir(Pessoa pessoa);
 
     Pessoa encontrarPessoa(Integer id);
+
+    Pessoa encontrarPessoaByUsuario(Usuario usuario);
 }
