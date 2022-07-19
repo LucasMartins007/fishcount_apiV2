ae9ee32cd2efc97648250cc41965b897d4f0caa3
Lucas Martins <lucasrodriguesmartinsoliveira@gmail.com>
2022-07-19 08:27:24 -0300

    Adicionado documentações dos endpoints de login

--- src/main/java/com/fishcount/api/controller/LoginController.java
+++ src/main/java/com/fishcount/api/controller/LoginController.java
@@ -3,7 +3,6 @@ package com.fishcount.api.controller;
 import com.fishcount.api.controller.interfaces.ILoginController;
 import com.fishcount.api.service.LoginService;
 import com.fishcount.common.model.dto.AutenticacaoDTO;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
  * @author lucas
  */
 @RestController
-@RequiredArgsConstructor
 public class LoginController extends AbstractController<LoginService> implements ILoginController {
 
 
     @Override
-    public ResponseEntity<AutenticacaoDTO> authenticate(@RequestBody AutenticacaoDTO autenticacaoDTO) {
+    public ResponseEntity<AutenticacaoDTO> autenticar(@RequestBody AutenticacaoDTO autenticacaoDTO) {
         AutenticacaoDTO autenticacao = getService().autenticar(autenticacaoDTO);
 
         return ResponseEntity.ok(autenticacao);
--- src/main/java/com/fishcount/api/controller/interfaces/ILoginController.java
+++ src/main/java/com/fishcount/api/controller/interfaces/ILoginController.java
@@ -2,12 +2,11 @@ package com.fishcount.api.controller.interfaces;
 
 import com.fishcount.common.model.dto.AutenticacaoDTO;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  *
@@ -16,14 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = ILoginController.PATH)
 @Api(tags = ILoginController.TAG)
-@Tag(name = ILoginController.TAG, description = "Autenticação")
+@Tag(name = ILoginController.TAG, description = ILoginController.DESCRIPTION)
 public interface ILoginController {
 
     String PATH = "login";
 
     String TAG = "Login";
 
+    String DESCRIPTION = "Endpoints referentes a autenticação da aplicação";
+
     @PostMapping
-    ResponseEntity<AutenticacaoDTO> authenticate(@RequestBody AutenticacaoDTO authenticationRequest);
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @ApiOperation(value = "${controller.login.autenticar.operation}", notes = "${controller.login.autenticar.description}")
+    ResponseEntity<AutenticacaoDTO> autenticar(@RequestBody AutenticacaoDTO autenticacaoDTO);
 
 }
