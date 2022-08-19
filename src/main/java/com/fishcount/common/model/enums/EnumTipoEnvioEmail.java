6d8ff13417c79db051d8404068499ac3a29267a4
Lucas Martins <lucasrodriguesmartinsoliveira@gmail.com>
2022-08-19 17:10:10 -0300

    Adicionado configuração para envio de email de suporte e cliente

--- src/main/java/com/fishcount/common/model/enums/EnumTipoEnvioEmail.java
+++ src/main/java/com/fishcount/common/model/enums/EnumTipoEnvioEmail.java
@@ -9,13 +9,13 @@ import javax.persistence.Converter;
 @Getter
 public enum EnumTipoEnvioEmail implements IEnum<Integer> {
 
-    RECUPERACAO_SENHA(1, "Solicitação de alteração de senha", true),
+    RECUPERACAO_SENHA(1, "Solicitação de alteração de senha", true, false),
 
-    CONFIRMACAO_NOVO_CADASTRO(2, "Novo cadastro", true),
+    CONFIRMACAO_NOVO_CADASTRO(2, "Novo cadastro", true, false),
 
-    CONFIRMACAO_EMAIL(3, "Confirmação de email", true),
+    CONFIRMACAO_EMAIL(3, "Confirmação de email", true, false),
 
-    EXCECAO_SISTEMA(4, "Exceção no sistema", true),
+    EXCECAO_SISTEMA(4, "Exceção no sistema", true, true),
     ;
 
     private final Integer key;
@@ -24,10 +24,13 @@ public enum EnumTipoEnvioEmail implements IEnum<Integer> {
 
     private final boolean html;
 
-    EnumTipoEnvioEmail(Integer key, String value, boolean html) {
+    private final boolean isSuporte;
+
+    EnumTipoEnvioEmail(Integer key, String value, boolean html, boolean isSuporte) {
         this.key = key;
         this.value = value;
         this.html = html;
+        this.isSuporte = isSuporte;
     }
 
     @Converter
