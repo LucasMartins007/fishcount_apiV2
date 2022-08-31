6920c3bea113ea2d5f0e9c9bbfd80b269baa3348
Lucas Martins <lucasrodriguesmartinsoliveira@gmail.com>
2022-08-31 13:16:36 -0300

    Adicionado scprits de inserção de parametros para cálculo dos valores da análise

---
+++ src/main/resources/db/migration/V1_2__insert_plano.sql
@@ -0,0 +1,14 @@
+
+INSERT INTO public.fin_plano
+(id, ativo, data_atualizacao, data_inclusao, descricao, max_taque, min_taque, qtde_parcela, valor_maximo, valor_minimo, valor_parcela_maximo, valor_parcela_minimo)
+VALUES(nextval('gen_id_fin_plano'), true, '2022-07-26 23:24:57.573', '2022-07-26 23:24:57.573', 'Corporativo', 300, 250, 10, 5000.00, 3500.00, 291.66, 83.33);
+
+
+INSERT INTO public.fin_plano
+(id, ativo, data_atualizacao, data_inclusao, descricao, max_taque, min_taque, qtde_parcela, valor_maximo, valor_minimo, valor_parcela_maximo, valor_parcela_minimo)
+VALUES(nextval('gen_id_fin_plano'), true, '2022-07-26 23:24:57.573', '2022-07-26 23:24:57.573', 'Executivo', 300, 250, 10, 3500.00, 1000.00,  500.00, 350.00);
+
+
+INSERT INTO public.fin_plano
+(id, ativo, data_atualizacao, data_inclusao, descricao, max_taque, min_taque, qtde_parcela, valor_maximo, valor_minimo, valor_parcela_maximo, valor_parcela_minimo)
+VALUES(nextval('gen_id_fin_plano'), true, '2022-07-26 23:24:57.573', '2022-07-26 23:24:57.573', 'Pessoal', 300, 250, 10, 1000.00, 600.00, 166.66, 100.00);
