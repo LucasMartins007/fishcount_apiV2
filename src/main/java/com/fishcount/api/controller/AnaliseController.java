515fa4188e780261039a8da717b1344bcf7656a0
Lucas Martins <lucasrodriguesmartinsoliveira@gmail.com>
2022-09-05 09:56:21 -0300

    Adicionado documentação dos endpoints da análise

--- src/main/java/com/fishcount/api/controller/AnaliseController.java
+++ src/main/java/com/fishcount/api/controller/AnaliseController.java
@@ -6,6 +6,7 @@ import com.fishcount.common.model.pattern.constants.OperationsParam;
 import com.fishcount.common.model.pattern.constants.OperationsPath;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -25,8 +26,9 @@ public interface AnaliseController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    AnaliseDTO requisitarInicioAnalise(@RequestParam(name = "tanqueId", required = true) Integer tanqueId,
-                                       @RequestParam(name = "temperatura", required = false) Integer temperatura);
+    @ApiOperation(value = "${controller.analise.requisitar.operation}", notes = "${controller.analise.requisitar.description}")
+    AnaliseDTO requisitarInicioAnalise(@ApiParam("${controller.analise.tanqueId}") @RequestParam(name = "tanqueId", required = true) Integer tanqueId,
+                                       @ApiParam("${controller.analise.temperatura}") @RequestParam(name = "temperatura", required = false) Integer temperatura);
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
