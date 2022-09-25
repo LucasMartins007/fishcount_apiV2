73c15993ea0e2296651deac1f8491768156079e0
Lucas Martins <lucasrodriguesmartinsoliveira@gmail.com>
2022-09-24 22:51:46 -0300

    Ajuste nos testes unitários ja adicionados

--- src/main/java/com/fishcount/api/controller/impl/LocationPixControllerImpl.java
+++ src/main/java/com/fishcount/api/controller/impl/LocationPixControllerImpl.java
@@ -4,21 +4,25 @@ import com.fishcount.api.controller.LocationPixController;
 import com.fishcount.api.controller.pattern.AbstractController;
 import com.fishcount.api.service.LocationPixService;
 import com.fishcount.common.model.dto.financeiro.pix.QRCodePixDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class LocationPixControllerImpl
         extends AbstractController<LocationPixService>
         implements LocationPixController {
 
+    private final LocationPixService locationPixService;
+
     @Override
     public QRCodePixDTO encontrarPorLocation(Integer pessoaId, Integer locationId) {
-        return converterEntityParaDTO(getService().gerarQrCodePorLocation(pessoaId, locationId), QRCodePixDTO.class);
+        return converterEntityParaDTO(locationPixService.gerarQrCodePorLocation(pessoaId, locationId), QRCodePixDTO.class);
     }
 
     @Override
     public QRCodePixDTO encontrarPorParcela(Integer pessoaId, Integer pagamentoParcelaId) {
-        return converterEntityParaDTO(getService().gerarQrCodePorPagamentoParcela(pessoaId, pagamentoParcelaId), QRCodePixDTO.class);
+        return converterEntityParaDTO(locationPixService.gerarQrCodePorPagamentoParcela(pessoaId, pagamentoParcelaId), QRCodePixDTO.class);
     }
 
 }
--- src/test/java/com/fishcount/api/controller/AnaliseControllerImplTest.java
+++ src/test/java/com/fishcount/api/controller/AnaliseControllerImplTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
