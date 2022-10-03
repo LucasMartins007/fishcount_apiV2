ea16a4751ead22a3dd4655e3e5ab803cea164ee8
Lucas Martins <lucasrodriguesmartinsoliveira@gmail.com>
2022-10-03 20:14:38 -0300

    correção query taxa crescimento

--- src/main/resources/db/migration/V1_1__insert_taxa_crescimento.sql
+++ src/main/resources/db/migration/V1_1__insert_taxa_crescimento.sql
@@ -1,6 +1,6 @@
 INSERT INTO public.fish_taxa_crescimento
 (id, data_atualizacao, data_inclusao, intervalo, qtde_aumento, unidade_aumento, unidade_intervalo, id_especie, ativo)
-VALUES(nextval('gen_id_fish_especie'), '2022-05-23 00:00:00.000', '2022-05-23 00:00:00.000', 250, 2.00, 'CM', 'DIA', null, true);
+VALUES(nextval('gen_id_fish_taxa_crescimento'), '2022-05-23 00:00:00.000', '2022-05-23 00:00:00.000', 250, 2.00, 'CM', 'DIA', null, true);
 
 UPDATE public.fish_especie
 SET  id_taxa_crescimento = 1;
