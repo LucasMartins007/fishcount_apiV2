e5c7cd53b6cbf1d8de0a2cd154df8671a786b645
lucasmartins007 <lucasrodriguesmartinsoliveira@gmail.com>
2022-10-22 18:54:44 -0300

    Ajustes scripts sql de inserção de dados

--- src/main/resources/db/migration/V1_1__insert_taxa_crescimento.sql
+++ src/main/resources/db/migration/V1_1__insert_taxa_crescimento.sql
@@ -2,8 +2,6 @@ INSERT INTO public.fish_taxa_crescimento
 (id, data_atualizacao, data_inclusao, intervalo, qtde_aumento, unidade_aumento, unidade_intervalo, id_especie, ativo)
 VALUES(nextval('gen_id_fish_taxa_crescimento'), '2022-05-23 00:00:00.000', '2022-05-23 00:00:00.000', 250, 2.00, 'CM', 'DIA', null, true);
 
-UPDATE public.fish_especie
-SET  id_taxa_crescimento = 1;
-
 UPDATE public.fish_taxa_crescimento
-SET  id_especie = 1;
+    SET  id_especie = 1
+    WHERE ID = 1;
--- src/main/resources/db/migration/V1_2__insert_plano.sql
+++ src/main/resources/db/migration/V1_2__insert_plano.sql
@@ -12,3 +12,7 @@ VALUES(nextval('gen_id_fin_plano'), true, '2022-07-26 23:24:57.573', '2022-07-26
 INSERT INTO public.fin_plano
 (id, ativo, data_atualizacao, data_inclusao, descricao, max_taque, min_taque, qtde_parcela, valor_maximo, valor_minimo, valor_parcela_maximo, valor_parcela_minimo)
 VALUES(nextval('gen_id_fin_plano'), true, '2022-07-26 23:24:57.573', '2022-07-26 23:24:57.573', 'Pessoal', 300, 250, 10, 1000.00, 600.00, 166.66, 100.00);
+
+UPDATE public.fish_especie
+    SET  id_taxa_crescimento = 1
+    WHERE ID = 1;
