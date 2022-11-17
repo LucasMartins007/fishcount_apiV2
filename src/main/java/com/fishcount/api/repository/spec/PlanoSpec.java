2a08ea104df1c8ce2a02b8aa7952686b5acc5cc0
lucasmartins007 <lucasrodriguesmartinsoliveira@gmail.com>
2022-11-17 20:37:48 -0300

    adicionado order by na listagem de planos

--- src/main/java/com/fishcount/api/repository/spec/PlanoSpec.java
+++ src/main/java/com/fishcount/api/repository/spec/PlanoSpec.java
@@ -5,6 +5,8 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
 
+import javax.persistence.criteria.Order;
+
 /**
  *
  * @author Lucas Martins
@@ -16,5 +18,15 @@ public class PlanoSpec {
 
     public static Specification<Plano> byAtivoTrue() {
         return (root, query, criteriaBuilder) -> criteriaBuilder.isTrue(root.get(FIELD_ATIVO));
+    }
+
+    public static Specification<Plano> orderBy(boolean isAscending, String campo) {
+        return (root, query, criteriaBuilder) -> {
+            Order order = isAscending ? criteriaBuilder.asc(root.get(campo)) : criteriaBuilder.desc(root.get(campo));
+
+            return query
+                    .distinct(true)
+                    .orderBy(order).getRestriction();
+        };
     }
 }
